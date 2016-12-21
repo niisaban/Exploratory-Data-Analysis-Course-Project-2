5e4221b123e36c6af34397ca43c094407393eed9
niisaban <abdulhammond@hotmail.com>
2016-12-21 01:21:34 -0500

    Changes to line 18
    In order to show plots (two grids only) that compare data from only Baltimore City and those from Los Angeles, 
    I removed the margins = TRUE from line 18. Previously, line 18 read: g <- g + facet_grid(. ~ fips, margins +TRUE).
    This created 3 grids namely: Baltimore City, MD, Los Angeles, CA and ALL.
    For 2 grid only, the proper syntax is: g <- g + facet_grid(. ~ fips).

--- plot 6.R
+++ plot 6.R
@@ -15,7 +15,7 @@ aggTotalByYearAndFips <- aggregate(Emissions ~ year + fips, NEIsub3, sum)
 aggTotalByYearAndFips$fips[aggTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
 aggTotalByYearAndFips$fips[aggTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
 g <- ggplot(aggTotalByYearAndFips, aes(factor(year), Emissions))
-g <- g + facet_grid(. ~ fips, margins = TRUE)
+g <- g + facet_grid(. ~ fips)
 g <- g + geom_bar(stat="identity") + xlab("year") + ylab(expression('Total PM'[2.5]*" Emissions in tons")) + ggtitle('Variations in Total Emissions (in tons) from motor vehicles: Baltimore City, MD vs Los Angeles, CA 1999-2008')
 print(g)
 dev.copy(png, file="plot6.png", height=1040, width=640)
